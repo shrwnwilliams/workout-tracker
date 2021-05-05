e6c43676efcb18b6afda3b0f99d91b1596fb967f
DINNERman <shrwnwilliams@gmail.com>
2021-05-05 16:21:29 -0400

    added the exercise and workout models, exported them in the index, and seeded the database successfully

---
+++ models/Excercise.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  type: {
+      type: String,
+      trim: true,
+      required: "Exercise type is required"
+    },
+
+    name: {
+        type: String,
+        trim: true,
+        required: "Exercise name is required"
+    },
+
+    duration: {
+        type: Number,
+        required: "Exercise duration is required"
+    },
+
+    weight:Number,
+
+    distance: Number,
+
+    reps: Number,
+
+    sets: Number
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Exercise: require("./Excercise"),
+    Workout: require("./Workout")
+};
