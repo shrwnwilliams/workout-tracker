9a42aeb2d3f0d9eb17a2f96bb31bd6935e43fe79
DINNERman <shrwnwilliams@gmail.com>
2021-05-05 17:28:48 -0400

    realized im setting up api routes

--- server.js
+++ server.js
@@ -21,10 +21,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // // routes go here
-// app.get("/", (req, res) => {
-
-//     res.sendFile(path.join(__dirname, 'public/index.html'));
-// })
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}, (err, found) =>{
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(found);
+        }
+    })
+})
 
 app.get("/stats", (req, res) =>{
 
