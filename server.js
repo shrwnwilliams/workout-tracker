c73bff25a9b924151fd536b6e2cee16ba8e105b4
DINNERman <shrwnwilliams@gmail.com>
2021-05-05 16:54:18 -0400

    added routes to get to all the provided html files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path")
 
 const PORT = process.env.PORT || 5555;
 
@@ -13,13 +14,27 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
 
-// routes go here
+// // routes go here
+// app.get("/", (req, res) => {
+
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+// })
+
+app.get("/stats", (req, res) =>{
+
+    res.sendFile(path.join(__dirname, 'public/stats.html'));
+})
+
+app.get("/exercise", (req, res) =>{
+
+    res.sendFile(path.join(__dirname, 'public/exercise.html'));
+})
 
 
 app.listen(PORT, () => {
