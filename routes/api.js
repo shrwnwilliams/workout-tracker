57ca1e19ca61d2a726901e15b1d66699ad9fa6cf
DINNERman <shrwnwilliams@gmail.com>
2021-05-06 13:47:39 -0400

    made the add and complete api routes work

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,28 @@
 // // routes go here
+const { Workout } = require("../models");
 const db = require("../models");
 const router = require("express").Router();
 
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find({}, (err, found) =>{
-        if (err) {
-            console.log(err)
-        } else {
-            res.json(found);
-        }
-    })
-})
+  db.Workout.find({}, (err, found) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(found);
+    }
+  });
+});
 
-router.post("/api/workouts/:id", (req, res) => {
-    
-})
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+//   const workout = new Workout(body);
+
+  db.Workout.findByIdAndUpdate(params.id, { $push: {exercises: body} }, { new: true })
+    .then((newWorkout) => {
+      res.json(newWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.use(require("./routes/api"));
 app.use(require("./routes/view"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port http://localhost:${PORT}`);
   });
   
